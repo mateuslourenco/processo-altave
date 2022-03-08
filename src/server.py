eeb1977c32102a3c3667da5ccc22f2b423e9ac5e
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-08 02:21:30 -0300

    implementada logica para criar usuario

--- src/server.py
+++ src/server.py
@@ -3,6 +3,11 @@ from datetime import datetime
 from fastapi import FastAPI
 from starlette.requests import Request
 
+from src.infra.sqlalchemy.config.database import criar_db
+from src.routers import rotas_auth
+
+criar_db()
+
 app = FastAPI()
 
 
@@ -12,6 +17,8 @@ async def horario_da_requisicao(request: Request, call_next):
     request.state.horario_requisicao = horario
     response = await call_next(request)
     return response
+
+app.include_router(rotas_auth.router, prefix='/auth')
 
 
 @app.get('/')
