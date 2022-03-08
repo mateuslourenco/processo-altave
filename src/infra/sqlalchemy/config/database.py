73e49d2f9f4803d719cab2d518f79e94b392fe78
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-08 01:25:18 -0300

    criado setup para db

---
+++ src/infra/sqlalchemy/config/database.py
@@ -0,0 +1,30 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+db_url = os.environ.get('DATABASE_URL', 'sqlite:///./altave.db')
+
+
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URL = db_url
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def criar_db():
+    Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
