ba9663e9da15dc36fa5092bc64087b2e8bcb2c0a
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-07 23:54:32 -0300

    implementado middleware para receber o horario da requisicao

--- src/server.py
+++ src/server.py
@@ -1,11 +1,19 @@
 from datetime import datetime
 
 from fastapi import FastAPI
+from starlette.requests import Request
 
 app = FastAPI()
 
 
-@app.get('/')
-def home():
+@app.middleware('http')
+async def horario_da_requisicao(request: Request, call_next):
     horario = datetime.now().strftime('%H:%M:%S')
-    return {'msg': horario}
+    request.state.horario_requisicao = horario
+    response = await call_next(request)
+    return response
+
+
+@app.get('/')
+def home(request: Request):
+    return {'horário': request.state.horario_requisicao}
