d61fb3b06a57c423ff40fbda14c9e4968667479b
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-07 23:01:49 -0300

    implementado horario na resposta http

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from starlette.testclient import TestClient
 
 from src.server import app
@@ -7,5 +9,6 @@ client = TestClient(app)
 
 def test_status_code():
     response = client.get('/')
+    horario = datetime.now().strftime('%H:%M:%S')
     assert response.status_code == 200
-    assert response.json() == {'msg': 'ALTAVE'}
+    assert response.json() == {'msg': horario}
